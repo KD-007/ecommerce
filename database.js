3776092c2cfcd2f83d4527932f38bd6e0fa7ae49
KD-007 <kuldeepverma.kv123@gmail.com>
2023-02-25 08:20:27 +0530

    final2

--- database.js
+++ database.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_URL).then(()=>{
+    mongoose.connect(process.env.DB_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }).then(()=>{
         console.log("database is connected");
     }).catch((e)=>{
         console.log("error connecting to database" ,e);
